fc0be1b8e4ca9567fab04765f50b18f224bf5ec9
pranong <nutpat2539@hotmail.com>
2018-05-16 17:07:40 +0700

    first commit

---
+++ android/app/src/main/java/com/aedproject/MainApplication.java
@@ -0,0 +1,66 @@
+package com.aedproject;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+// import org.reactnative.camera.RNCameraPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.airbnb.android.react.maps.MapsPackage;
+
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNCameraPackage(),
+            new VectorIconsPackage(),
+            new RNFetchBlobPackage(),
+            new PickerPackage(),
+          // new RNCameraPackage(),
+          new RNFirebasePackage(),
+          new MapsPackage(),
+          new RNFirebaseDatabasePackage(),
+          new RNFirebaseStoragePackage() 
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+
+}
